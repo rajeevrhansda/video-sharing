ef3811c9bcd0d3b02835dbca7c54c85599db8f42
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 20:21:17 +0530

    added video cards

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
-import { darkTheme, lightTheme } from './components/utils/Theme';
+import { darkTheme, lightTheme } from './utils/Theme';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './components/pages/Home';
-import Video from './components/pages/Video';
+import Home from './pages/Home';
+import Video from './pages/Video';
 
 const Container = styled.div`
 display: flex;
@@ -17,7 +17,7 @@ background-color: ${({ theme }) => theme.bg};
 `
 
 const Wrapper = styled.div`
-
+  padding: 22px 96px;
 `
 
 function App() {
