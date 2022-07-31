5e5d664f84bad2f28606883cd8864c18843044d6
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 18:39:39 +0530

    material icon

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
+import LamaTube from '../img/logo.png'
+import HomeIcon from '@mui/icons-material/Home';
 
 const Container = styled.div`
     flex: 1;
     background-color: #202020;
     height: 100vh;
     color: white;
-`
+    font-size: 14px;
+`;
 const Wrapper = styled.div`
+    padding: 18px 26px;
+`;
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-weight: bold;;
+    margin-bottom: 25px;
+`;
+const Img = styled.img`
+    height: 25px;
+`;
+const Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    cursor: pointer;
+`;
 
-`
-    
 
 function Menu() {
     return (
-        <Container></Container>
+        <Container>
+            <Wrapper>
+                <Logo>
+                    <Img src={LamaTube}/>
+                    LamaTube
+                </Logo>
+                <Item>
+                    <HomeIcon/>
+                    Home
+                </Item>
+            </Wrapper>
+        </Container>
     )
 }
 
