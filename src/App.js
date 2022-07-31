1a9e09ef9ff8cad34dd94886d9a3fd4f67e0b76b
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 19:53:00 +0530

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import styled, { ThemeProvider } from 'styled-components';
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
 import { darkTheme, lightTheme } from './components/utils/Theme';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import Video from './components/pages/Video';
 
 const Container = styled.div`
 display: flex;
@@ -22,13 +25,23 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Main>
-          <Navbar />
-          <Wrapper>
-            video card
-          </Wrapper>
-        </Main>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path='/'>
+                  <Route index element={<Home />} />
+                  <Route path='video'>
+                    <Route path=':id' element={<Video />} />
+                  </Route>
+
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
       </Container>
     </ThemeProvider>
   );
