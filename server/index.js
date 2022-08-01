4eaa93630ac6d4671c80191f6c9574f86cf94ebc
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-01 08:17:29 +0530

    basic mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
-import express from 'express'
+import mongoose from 'mongoose';
+import express from 'express';
 const app = express();
 
-app.listen(8800, ()=>{
+
+main()
+.then(console.log("DB Connect"))
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://admin:qweasd@cluster0-shard-00-00.k72iz.mongodb.net:27017,cluster0-shard-00-01.k72iz.mongodb.net:27017,cluster0-shard-00-02.k72iz.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-27vl2r-shard-0&authSource=admin&retryWrites=true&w=majority');
+};
+
+app.get('/', (req, res)=>{
+    res.send('MongoDB Server!')
+});
+
+app.listen(8800, () => {
     console.log("Server is running");
-})
+});
