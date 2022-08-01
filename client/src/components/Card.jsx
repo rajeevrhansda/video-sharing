83ba1a8e6df498d7b1bc895379d86977085d7386
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-02 01:10:52 +0530

    timeago.js format

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
+import {format} from 'timeago.js'
 
 const Container = styled.div`
     width: ${(props)=>props.type !== "sm" && "360px"};
@@ -62,7 +63,7 @@ const Card = ({type , video}) => {
                     <Texts>
                         <Title>{video?.title}</Title>
                         <ChannelName>Lama Dev</ChannelName>
-                        <Info>{video.views} views {video.createdAt}</Info>
+                        <Info>{video.views} views {format(video.createdAt)}</Info>
                     </Texts>
                 </Details>
             </Container>
