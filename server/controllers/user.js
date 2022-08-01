3b424f4e8d5412ba62d4a7a1bb967dea5596f5a4
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-01 09:24:04 +0530

    added routes, controllers, url res log

---
+++ server/controllers/user.js
@@ -0,0 +1,4 @@
+export const test = (req,res)=>{
+    res.json("test is working")
+    console.log("test is working");
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/users.js';
 const app = express();
 dotenv.config();
 
@@ -20,6 +21,8 @@ const connect = () => {
 app.get('/', (req, res) => {
     res.send('MongoDB Server!')
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(8800, () => {
     connect();
