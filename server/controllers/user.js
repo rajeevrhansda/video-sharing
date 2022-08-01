c9e16e79c2983b182d73d5a3d13c9bd9cb985206
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-01 12:27:20 +0530

    initialize user route controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,27 @@
-export const test = (req,res)=>{
-    res.json("test is working")
-    console.log("test is working");
+export const update = (req,res, next)=>{
+    
 }
+export const deleteUser = (req,res, next)=>{
+    
+}
+
+export const getUser = (req,res, next)=>{
+    
+}
+
+export const subscribe = (req,res, next)=>{
+    
+}
+
+export const unsubscribe = (req,res, next)=>{
+    
+}
+
+export const like = (req,res, next)=>{
+    
+}
+
+export const dislike = (req,res, next)=>{
+    
+}
+
