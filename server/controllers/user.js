fda491b5216b9a67ff3ed2f06a1280500031a2e4
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-01 18:52:30 +0530

    typo delete user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,8 +24,7 @@ export const update = async (req, res, next) => {
 export const deleteUser = async (req, res, next) => {
     if (req.params.id === req.user.id) {
         try {
-            await User.findByIdAndDelete
-            req.params.id,
+            await User.findByIdAndDelete(req.params.id);
                 res.status(200).json("User has been deleted");
         } catch (err) {
             next(err);
