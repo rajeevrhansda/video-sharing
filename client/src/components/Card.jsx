02880d6010797ff18b6d1abf3b200124b2f3b00f
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-02 01:15:07 +0530

    fetchchannel video card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 import {format} from 'timeago.js'
@@ -54,15 +55,24 @@ const Info = styled.div`
 
 
 const Card = ({type , video}) => {
+    const [channel, setChannel] = useState({});
+
+    useEffect(() => {
+        const fetchChannel = async () => {
+            const res = await axios.get(`/users/find/${video.userId}`);
+            setChannel(res.data);
+        }
+        fetchChannel();
+    }, [video.userId]);
     return (
         <Link to='/video/test'>
             <Container type={type}>
                 <Image type={type} src={video?.imgUrl} />
                 <Details type={type}>
-                    <ChannelImage type={type} src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+                    <ChannelImage type={type} src={channel.img} />
                     <Texts>
                         <Title>{video?.title}</Title>
-                        <ChannelName>Lama Dev</ChannelName>
+                        <ChannelName>{channel.name}</ChannelName>
                         <Info>{video.views} views {format(video.createdAt)}</Info>
                     </Texts>
                 </Details>
