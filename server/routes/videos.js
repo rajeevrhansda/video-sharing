2fc7444a8c1d420465bd90f12a770e5b35852cbd
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-01 21:19:08 +0530

    addVideo,     updateVideo,     deleteVideo,     getVideo,     addView,     random,     trend,     sub,     getByTag,     search

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,7 +1,41 @@
 import express from "express";
-import {  } from "../controllers/video.js";
+import {
+    addVideo,
+    updateVideo,
+    deleteVideo,
+    getVideo,
+    addView,
+    random,
+    trend,
+    sub,
+    getByTag,
+    search
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
 
 const router = express.Router();
+
+router.post("/", verifyToken, addVideo);
+
+router.put("/:id", verifyToken, addVideo);
+
+router.delete("/:id", verifyToken, addVideo);
+
+router.get("/find/:id", getVideo);
+
+router.put("/view/:id", addView);
+
+router.get("/trend", random);
+
+router.get("/random", trend);
+
+router.get("/sub", verifyToken, sub);
+
+router.get("/tags", getByTag);
+
+router.get("/search", search);
+
 
 
 export default router;
