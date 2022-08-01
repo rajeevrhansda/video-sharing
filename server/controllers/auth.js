c2c11c71d1e55e7189258a8449a9942f567515c5
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-01 10:22:09 +0530

    sign in check username and password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,20 +1,30 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
 
 export const signup = async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt)
-    try{
-        const newUser = new User({...req.body, password: hash});
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    try {
+        const newUser = new User({ ...req.body, password: hash });
         await newUser.save();
         res.status(200).send("User has been created");
-    }catch(err){
+    } catch (err) {
         next(err);
-    } 
+    }
 };
 
 export const signin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ name: req.body.name });
+        if (!user) return next(createError(404, "User not found"));
+
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
+        if (!isCorrect) return next(createError(404, "Wrong password"));
+    } catch (err) {
+        next(err);
+    }
 }
 
 export const googleAuth = async (req, res, next) => {
